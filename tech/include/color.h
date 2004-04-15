704660ad8d2f4f197047974d8177f6a73c3ced3e
Daniel Palma <daniel_palma@yahoo.com>
2004-04-15 21:28:25 +0000

    Made all cColor's methods inline

--- tech/include/color.h
+++ tech/include/color.h
@@ -4,8 +4,6 @@
 #ifndef INCLUDED_COLOR_H
 #define INCLUDED_COLOR_H
 
-#include "renderdll.h"
-
 #ifdef _MSC_VER
 #pragma once
 #endif
@@ -15,7 +13,7 @@
 // CLASS: cColor
 //
 
-class RENDER_API cColor
+class cColor
 {
 public:
    cColor();
@@ -30,6 +28,70 @@ public:
 private:
    float m_rgba[4];
 };
+
+///////////////////////////////////////
+
+inline cColor::cColor()
+{
+}
+
+///////////////////////////////////////
+
+inline cColor::cColor(float r, float g, float b)
+{
+   m_rgba[0] = r;
+   m_rgba[1] = g;
+   m_rgba[2] = b;
+   m_rgba[3] = 1;
+}
+
+///////////////////////////////////////
+
+inline cColor::cColor(float r, float g, float b, float a)
+{
+   m_rgba[0] = r;
+   m_rgba[1] = g;
+   m_rgba[2] = b;
+   m_rgba[3] = a;
+}
+
+///////////////////////////////////////
+
+inline cColor::cColor(const float rgba[4])
+{
+   m_rgba[0] = rgba[0];
+   m_rgba[1] = rgba[1];
+   m_rgba[2] = rgba[2];
+   m_rgba[3] = rgba[3];
+}
+
+///////////////////////////////////////
+
+inline cColor::cColor(const cColor & other)
+{
+   m_rgba[0] = other.m_rgba[0];
+   m_rgba[1] = other.m_rgba[1];
+   m_rgba[2] = other.m_rgba[2];
+   m_rgba[3] = other.m_rgba[3];
+}
+
+///////////////////////////////////////
+
+inline const cColor & cColor::operator =(const cColor & other)
+{
+   m_rgba[0] = other.m_rgba[0];
+   m_rgba[1] = other.m_rgba[1];
+   m_rgba[2] = other.m_rgba[2];
+   m_rgba[3] = other.m_rgba[3];
+   return *this;
+}
+
+///////////////////////////////////////
+
+inline const float * cColor::GetPointer() const
+{
+   return m_rgba;
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 
