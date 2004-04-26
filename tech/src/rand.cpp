c23a46c5ddcc795bf72fcf78a8a74d517b9a4797
Daniel Palma <daniel_palma@yahoo.com>
2004-04-26 16:38:35 +0000

    Added a unit test

--- tech/src/rand.cpp
+++ tech/src/rand.cpp
@@ -4,6 +4,11 @@
 #include "stdhdr.h"
 #include "techmath.h"
 
+#ifdef HAVE_CPPUNIT
+#include <cppunit/extensions/HelperMacros.h>
+#include <ctime>
+#endif
+
 #include "dbgalloc.h" // must be last header
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -21,5 +26,56 @@ uint Rand()
 {
    return g_a = g_a * g_b + 1;
 }
+
+///////////////////////////////////////////////////////////////////////////////
+
+#ifdef HAVE_CPPUNIT
+
+class cRandTests : public CppUnit::TestCase
+{
+   CPPUNIT_TEST_SUITE(cRandTests);
+      CPPUNIT_TEST(TestFrequency);
+   CPPUNIT_TEST_SUITE_END();
+
+   void TestFrequency();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(cRandTests);
+
+static uint CountOneBits(uint n)
+{
+   uint count = 0;
+   for (uint i = 0; i < (sizeof(uint) * 8); i++)
+   {
+      if ((1 << i) & n)
+      {
+         count++;
+      }
+   }
+   return count;
+}
+
+// Do a simple "frequency" test on the random number generator.
+// See if the bit stream composed of a bunch of random numbers is
+// within a tolerance of 50% ones.
+void cRandTests::TestFrequency()
+{
+   SeedRand(time(NULL));
+
+   const uint kNumTests = 1000;
+   uint nTotalBits = sizeof(uint) * kNumTests * 8;
+   uint nOneBits = 0;
+
+   for (int i = 0; i < kNumTests; i++)
+   {
+      nOneBits += CountOneBits(Rand());
+   }
+
+   double onesFraction = (double)nOneBits / nTotalBits;
+
+   CPPUNIT_ASSERT(fabs(onesFraction - 0.5) < 0.01);
+}
+
+#endif // HAVE_CPPUNIT
 
 ///////////////////////////////////////////////////////////////////////////////
