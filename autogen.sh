c3c90c7134f51937e1a4e4e81dcefcfbf4507d44
Daniel Palma <daniel_palma@yahoo.com>
2004-08-09 18:13:18 +0000

    Attempt to detect autoconf tools not being installed

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,26 @@
+#!/bin/sh
 # $Id$
-#! /bin/sh
+
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have autoconf installed to build this project."
+	exit 1
+}
+
+(libtool --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have libtool installed to build this project."
+	exit 1
+}
+
+(automake --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have automake installed to build this project."
+	exit 1
+}
+
 aclocal -I ./config
-libtoolize --force --copy
-autoheader 
-automake --add-missing --copy
-autoconf
+    && libtoolize --force --copy
+    && autoheader 
+    && automake --add-missing --copy
+    && autoconf
