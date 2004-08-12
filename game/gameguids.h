9f0002c573ab5064e3cc4c8790ad7ad0c498e7e2
Daniel Palma <daniel_palma@yahoo.com>
2004-08-12 20:08:27 +0000

    Removed old UI code

--- game/gameguids.h
+++ game/gameguids.h
@@ -10,18 +10,6 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 
-// {59013351-8C9A-419c-AE0B-A834EDE233D9}
-DEFINE_GUID(IID_IUIStyle, 
-0x59013351, 0x8c9a, 0x419c, 0xae, 0xb, 0xa8, 0x34, 0xed, 0xe2, 0x33, 0xd9);
-
-// {D7007F7B-431B-459e-819B-B194EA09EF6A}
-DEFINE_GUID(IID_IUIEventListener, 
-0xd7007f7b, 0x431b, 0x459e, 0x81, 0x9b, 0xb1, 0x94, 0xea, 0x9, 0xef, 0x6a);
-
-// {F9ED4FD8-4A47-4236-A10F-2CA05D530534}
-DEFINE_GUID(IID_IUIRenderingTools, 
-0xf9ed4fd8, 0x4a47, 0x4236, 0xa1, 0xf, 0x2c, 0xa0, 0x5d, 0x53, 0x5, 0x34);
-
 ///////////////////////////////////////////////////////////////////////////////
 
 #endif // !INCLUDED_GAMEGUIDS_H
