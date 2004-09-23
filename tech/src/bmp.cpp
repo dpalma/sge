0da9f6106c177774470b0bf584fa5375cf5582ed
Daniel Palma <daniel_palma@yahoo.com>
2004-09-23 03:48:12 +0000

    Do some work to undo the DWORD-aligning in the BMP file to be consistent with the Targa loader

--- tech/src/bmp.cpp
+++ tech/src/bmp.cpp
@@ -52,36 +52,78 @@ cImageData * LoadBmp(IReader * pReader)
    if (pReader->Read(&header, sizeof(header)) != S_OK ||
        header.bfType != 0x4D42 ||
        pReader->Read(&info, sizeof(info)) != S_OK)
-      return NULL;
-
-   ulong memSize = header.bfSize - header.bfOffBits;
-
-   byte * pImageData = new byte[memSize];
-
-   pReader->Seek(header.bfOffBits, kSO_Set);
-
-   if (pReader->Read(pImageData, memSize) != S_OK)
    {
-      DebugMsg("Failed to read BMP image data\n");
-      delete [] pImageData;
-      return false;
+      return NULL;
    }
 
    if (info.biBitCount != 24)
    {
       DebugMsg1("Un-supported BMP image format (%d bits per pixel)\n", info.biBitCount);
-      delete [] pImageData;
       return NULL;
    }
 
-   cImageData * pImage = new cImageData;
-   if (!pImage->Create(info.biWidth, info.biHeight, kPF_BGR888, pImageData))
-   {
-      delete pImage;
-      pImage = NULL;
-   }
+   cImageData * pImage = NULL;
 
-   delete [] pImageData;
+   ulong memSize = header.bfSize - header.bfOffBits;
+
+   byte * pAlignedImageBits = new byte[memSize];
+
+   if (pAlignedImageBits != NULL)
+   {
+      // The image scan-lines image in the file will be aligned on 4-byte
+      // boundaries. Do work here to remove the padding. Will mean more work
+      // later if this image data will be used to create Windows HBITMAPs,
+      // but will make it consistent with the Targa loader and the image client
+      // code that has been written thus far.
+      if (pReader->Seek(header.bfOffBits, kSO_Set) == S_OK
+         && pReader->Read(pAlignedImageBits, memSize) == S_OK)
+      {
+         size_t unalignedMemSize = info.biWidth * info.biHeight * info.biBitCount / 8;
+
+         byte * pUnalignedImageBits = new byte[unalignedMemSize];
+
+         if (pUnalignedImageBits != NULL)
+         {
+            uint bytesPerPixel = 3; // only support 24 bit (24/8 = 3)
+
+            size_t unalignedScanLine = info.biWidth * bytesPerPixel;
+            size_t alignedScanLine = memSize / info.biHeight;
+
+            if (unalignedScanLine == alignedScanLine)
+            {
+               pImage = new cImageData;
+               if (!pImage->Create(info.biWidth, info.biHeight, kPF_BGR888, pAlignedImageBits))
+               {
+                  delete pImage;
+                  pImage = NULL;
+               }
+            }
+            else
+            {
+               byte * pAligned = pAlignedImageBits;
+               byte * pUnaligned = pUnalignedImageBits;
+
+               for (int i = 0; i < info.biHeight; i++)
+               {
+                  memcpy(pUnaligned, pAligned, unalignedScanLine);
+                  pUnaligned += unalignedScanLine;
+                  pAligned += alignedScanLine;
+               }
+
+               pImage = new cImageData;
+               if (!pImage->Create(info.biWidth, info.biHeight, kPF_BGR888, pUnalignedImageBits))
+               {
+                  delete pImage;
+                  pImage = NULL;
+               }
+            }
+
+            delete [] pUnalignedImageBits;
+         }
+      }
+
+      delete [] pAlignedImageBits;
+   }
 
    return pImage;
 }
