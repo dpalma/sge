e797ba73b8e637ebc70d4438a33cc765f8d52425
Daniel Palma <daniel_palma@yahoo.com>
2004-10-07 16:46:43 +0000

    Added ToggleFrameStats command

--- game/cmds.cpp
+++ game/cmds.cpp
@@ -9,6 +9,8 @@
 #include "scriptvar.h"
 #include "guiapi.h"
 
+#include "render.h"
+
 #include "globalobj.h"
 
 #include "dbgalloc.h" // must be last header
@@ -39,5 +41,56 @@ SCRIPT_DEFINE_FUNCTION(ConfirmedQuit)
 
    return 0;
 }
+
+///////////////////////////////////////////////////////////////////////////////
+
+extern IRenderDevice * AccessRenderDevice();
+
+int ToggleFrameStats(int argc, const cScriptVar * argv, 
+                     int nMaxResults, cScriptVar * pResults)
+{
+   float x = 25, y = 25;
+   tGUIColor color(tGUIColor::White);
+
+   if (argc == 2 
+      && ScriptArgIsNumber(0) 
+      && ScriptArgIsNumber(1))
+   {
+      x = ScriptArgAsNumber(0);
+      y = ScriptArgAsNumber(1);
+   }
+   else if (argc == 3 
+      && ScriptArgIsNumber(0) 
+      && ScriptArgIsNumber(1) 
+      && ScriptArgIsString(2))
+   {
+      x = ScriptArgAsNumber(0);
+      y = ScriptArgAsNumber(1);
+      GUIStyleParseColor(ScriptArgAsString(2), &color);
+   }
+   else if (argc == 5 
+      && ScriptArgIsNumber(0) 
+      && ScriptArgIsNumber(1) 
+      && ScriptArgIsNumber(2) 
+      && ScriptArgIsNumber(3) 
+      && ScriptArgIsNumber(4))
+   {
+      x = ScriptArgAsNumber(0);
+      y = ScriptArgAsNumber(1);
+      color = tGUIColor(ScriptArgAsNumber(2), ScriptArgAsNumber(3), ScriptArgAsNumber(4));
+   }
+
+   if (AccessRenderDevice() != NULL)
+   {
+      if (AccessRenderDevice()->ShowStatistics(x, y, color) == S_FALSE)
+      {
+         AccessRenderDevice()->HideStatistics();
+      }
+   }
+
+   return 0;
+}
+
+AUTOADD_SCRIPTFUNCTION(ToggleFrameStats, ToggleFrameStats);
 
 ///////////////////////////////////////////////////////////////////////////////
