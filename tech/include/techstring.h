a0c4e2b79e616cb7ddaa3a50eb0f0683d394f9d8
Daniel Palma <daniel_palma@yahoo.com>
2005-05-04 05:44:01 +0000

    Starting to tighten up the cStr class; New header techstr.h will eventually replace str.h

--- tech/include/techstring.h
+++ tech/include/techstring.h
@@ -1,8 +1,8 @@
 ///////////////////////////////////////////////////////////////////////////////
 // $Id$
 
-#ifndef INCLUDED_STR_H
-#define INCLUDED_STR_H
+#ifndef INCLUDED_TECHSTRING_H
+#define INCLUDED_TECHSTRING_H
 
 #include "techdll.h"
 
@@ -17,24 +17,38 @@
 // CLASS: cStr
 //
 
-#ifdef _UNICODE
-typedef std::wstring cStrBase;
-#else
-typedef std::string cStrBase;
-#endif
-
-class cStr : public cStrBase
+class cStr : private std::string
 {
+   typedef std::string tBase;
+
 public:
    cStr();
    cStr(const tChar * psz);
+   cStr(const tChar * psz, uint length);
 
    operator const tChar *() const;
 
-   int GetLength() const;
+   bool operator ==(const cStr & other) const;
+   bool operator !=(const cStr & other) const;
+   bool operator ==(const tChar * psz) const;
+   bool operator !=(const tChar * psz) const;
 
+   const tChar * Get() const;
+   int GetLength() const;
    void Empty();
+   bool IsEmpty() const;
+
+   void Append(const tChar * psz);
+   void TrimLeadingSpace();
+   void TrimTrailingSpace();
+
+   int ToInt() const;
+   float ToFloat() const;
+
+   static const cStr gm_whitespace;
 };
+
+__declspec(selectany) const cStr cStr::gm_whitespace(" \t\r\n");
 
 ///////////////////////////////////////
 
@@ -45,7 +59,14 @@ inline cStr::cStr()
 ///////////////////////////////////////
 
 inline cStr::cStr(const tChar * psz)
- : cStrBase(psz)
+ : tBase(psz)
+{
+}
+
+///////////////////////////////////////
+
+inline cStr::cStr(const tChar * psz, uint length)
+ : tBase(psz, length)
 {
 }
 
@@ -53,23 +74,123 @@ inline cStr::cStr(const tChar * psz)
 
 inline cStr::operator const tChar *() const
 {
-   return cStrBase::c_str();
+   return Get();
+}
+
+///////////////////////////////////////
+
+inline bool cStr::operator ==(const cStr & other) const
+{
+   return compare(other) == 0;
+}
+
+///////////////////////////////////////
+
+inline bool cStr::operator !=(const cStr & other) const
+{
+   return compare(other) != 0;
+}
+
+///////////////////////////////////////
+
+inline bool cStr::operator ==(const tChar * psz) const
+{
+   return compare(psz) == 0;
+}
+
+///////////////////////////////////////
+
+inline bool cStr::operator !=(const tChar * psz) const
+{
+   return compare(psz) != 0;
+}
+
+///////////////////////////////////////
+
+inline const tChar * cStr::Get() const
+{
+   return c_str();
 }
 
 ///////////////////////////////////////
 
 inline int cStr::GetLength() const
 {
-   return cStrBase::length();
+   return length();
 }
 
 ///////////////////////////////////////
 
 inline void cStr::Empty()
 {
-   cStrBase::erase();
+   erase();
+}
+
+///////////////////////////////////////
+
+inline bool cStr::IsEmpty() const
+{
+   return empty();
+}
+
+///////////////////////////////////////
+
+inline void cStr::Append(const tChar * psz)
+{
+   *this += psz;
+}
+
+///////////////////////////////////////
+
+inline void cStr::TrimLeadingSpace()
+{
+   tBase::size_type index = find_first_not_of(gm_whitespace);
+   if (index != tBase::npos)
+   {
+      erase(0, index);
+   }
+}
+
+///////////////////////////////////////
+
+inline void cStr::TrimTrailingSpace()
+{
+   tBase::size_type index = find_last_not_of(gm_whitespace);
+   if (index != tBase::npos)
+   {
+      erase(index + 1);
+   }
+}
+
+///////////////////////////////////////
+
+inline int cStr::ToInt() const
+{
+   return atoi(Get());
+}
+
+///////////////////////////////////////
+
+inline float cStr::ToFloat() const
+{
+   return static_cast<float>(atof(Get()));
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
-#endif // !INCLUDED_STR_H
+class cStrLessNoCase
+{
+public:
+   bool operator()(const cStr & lhs, const cStr & rhs) const;
+};
+
+///////////////////////////////////////
+
+inline bool cStrLessNoCase::operator()(const cStr & lhs, const cStr & rhs) const
+{
+   return (stricmp(lhs, rhs) < 0) ? true : false;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+#endif // !INCLUDED_TECHSTRING_H
