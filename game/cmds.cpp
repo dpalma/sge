80aaed771e81196db75ab885c217ae3fbbe9b655
Daniel Palma <daniel_palma@yahoo.com>
2005-06-03 14:06:29 +0000

    Fixed ConfirmedQuit() script function

--- game/cmds.cpp
+++ game/cmds.cpp
@@ -9,8 +9,6 @@
 #include "scriptvar.h"
 #include "guiapi.h"
 
-#include "renderapi.h"
-
 #include "globalobj.h"
 
 #include "dbgalloc.h" // must be last header
@@ -30,9 +28,9 @@ SCRIPT_DEFINE_FUNCTION(ConfirmedQuit)
    if (ScriptArgc() == 1 && ScriptArgIsString(0))
    {
       UseGlobal(GUIContext);
-      if (pGUIContext->LoadFromString(ScriptArgAsString(0)) == 0)
+      if (pGUIContext->LoadFromString(ScriptArgAsString(0)) != S_OK)
       {
-         if (pGUIContext->LoadFromResource(ScriptArgAsString(0)) == 0)
+         if (pGUIContext->LoadFromResource(ScriptArgAsString(0)) != S_OK)
          {
             DebugMsg1("Error showing quit dialog %s\n", ScriptArgAsString(0));
          }
