7d5b59741b9e24ce0a14baa7c372ef295ae38bb8
Daniel Palma <daniel_palma@yahoo.com>
2006-04-28 20:41:20 +0000

    Removed some obsolete stuff from the resource API

--- tech/include/resourceapi.h
+++ tech/include/resourceapi.h
@@ -35,79 +35,6 @@ TECH_API tResult TextFormatRegister(const tChar * pszExtension);
 
 ///////////////////////////////////////////////////////////////////////////////
 //
-// CLASS: cResourceKey
-//
-
-class cResourceKey
-{
-public:
-   cResourceKey(const tChar * pszName, const tChar * pszType);
-   cResourceKey(const cResourceKey & other);
-
-   const cResourceKey & operator =(const cResourceKey & other);
-
-   bool operator ==(const cResourceKey & other);
-
-   const tChar * GetName() const;
-   const tChar * GetType() const;
-
-private:
-   cStr m_name, m_type;
-};
-
-///////////////////////////////////////
-
-typedef cResourceKey tResKey;
-
-///////////////////////////////////////
-
-inline cResourceKey::cResourceKey(const tChar * pszName, const tChar * pszType)
- : m_name(pszName != NULL ? pszName : _T("")),
-   m_type(pszType != NULL ? pszType : _T(""))
-{
-}
-
-///////////////////////////////////////
-
-inline cResourceKey::cResourceKey(const cResourceKey & other)
- : m_name(other.m_name),
-   m_type(other.m_type)
-{
-}
-
-///////////////////////////////////////
-
-inline const cResourceKey & cResourceKey::operator =(const cResourceKey & other)
-{
-   m_name = other.m_name;
-   m_type = other.m_type;
-   return *this;
-}
-
-///////////////////////////////////////
-
-inline bool cResourceKey::operator ==(const cResourceKey & other)
-{
-   return (m_name == other.m_name) && (m_type == other.m_type);
-}
-
-///////////////////////////////////////
-
-inline const tChar * cResourceKey::GetName() const
-{
-   return m_name.c_str();
-}
-
-///////////////////////////////////////
-
-inline const tChar * cResourceKey::GetType() const
-{
-   return m_type.c_str();
-}
-
-
-///////////////////////////////////////////////////////////////////////////////
-//
 // INTERFACE: IResourceManager
 //
 
@@ -126,21 +53,6 @@ interface IResourceManager : IUnknown
 
    virtual tResult Load(const tChar * pszName, tResourceType type, void * param, void * * ppData) = 0;
    virtual tResult Unload(const tChar * pszName, tResourceType type) = 0;
-
-   tResult Load(const tResKey & key, void * param, void * * ppData)
-   {
-      return Load(key.GetName(), key.GetType(), param, ppData);
-   }
-
-   tResult Load(const tResKey & key, void * * ppData)
-   {
-      return Load(key, NULL, ppData);
-   }
-
-   tResult Unload(const tResKey & key)
-   {
-      return Unload(key.GetName(), key.GetType());
-   }
 
    virtual tResult RegisterFormat(tResourceType type,
                                   tResourceType typeDepend,
