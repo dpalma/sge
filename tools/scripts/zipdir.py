ab98e0e97b71b04175aa12625815bc950ad0945d
Daniel Palma <daniel_palma@yahoo.com>
2007-04-13 18:57:01 +0000

    Added fixes and new command-line options to zipdir.py to make it usable from a Visual C++ post-build step

--- tools/scripts/zipdir.py
+++ tools/scripts/zipdir.py
@@ -7,16 +7,17 @@ from Walk import Walk
 from zipfile import *
 
 def usage():
-	print sys.argv[0] + ' --dir (Directory to zip) [--help]'
+	print sys.argv[0] + ' --dir (Directory to zip) [--zip (Name of the zip file)] [--help]'
 	
 def main(argv):
 	try:
-		opts, args = getopt(argv, "hd:", ["help", "dir="])
+		opts, args = getopt(argv, "hd:z:", ["help", "dir=", "zip="])
 	except GetoptError:
 		usage()
 		return -1
 		
 	dir = None
+	zipName = None
 	
 	for opt, arg in opts:
 		if opt in ("-h", "--help"):
@@ -24,6 +25,8 @@ def main(argv):
 			return 0
 		elif opt in ("-d", "--dir"):
 			dir = arg
+		elif opt in ("-z", "--zip"):
+			zipName = arg
 		else:
 			print >> sys.stderr, 'Unknown argument: ' + opt
 			return -1
@@ -36,16 +39,17 @@ def main(argv):
 		print >> sys.stderr, dir + ' is not a directory'
 		return -1
 	
-	zname = dir + os.extsep + 'zip'
-	z = ZipFile(zname, 'w', ZIP_DEFLATED)
+	dir = os.path.normpath(dir)
+	
+	if zipName is None:
+		zipName = dir + os.extsep + 'zip'
+	z = ZipFile(zipName, 'w', ZIP_DEFLATED)
 	
 	files = Walk(dir, 1)
 	for f in files:
 		if f.find('.svn') < 0:
 			if f.startswith(dir):
-				l = f.split(os.sep)
-				l.remove(dir)
-				aname = os.sep.join(l)
+				aname = f.replace(dir, '').lstrip(os.sep)
 				z.write(f, aname)
 				
 	z.close()
